276cfd3f485ece6c434b6b5cc9cb22ca03a7bddd
Dinuka7L <dinoo.walker7@gmail.com>
2026-02-05 14:36:35 +0530

    Gh-pages-fix-v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  base: '/Flurry-RSVP-Reader/',
+  base: process.env.NODE_ENV === 'production' ? '/Flurry-RSVP-Reader/' : '/',
 });
