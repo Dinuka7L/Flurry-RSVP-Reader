665863be35549a0db78b60889e7d81bbf37d5e94
Dinuka7L <dinoo.walker7@gmail.com>
2026-02-05 14:46:54 +0530

    Gh-pages-fix-v6

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Flurry-RSVP-Reader/' : '/',
+  base: process.env.NODE_ENV === 'production' ? 'https://github.com/Dinuka7L/Flurry-RSVP-Reader/' : '/',
 });
